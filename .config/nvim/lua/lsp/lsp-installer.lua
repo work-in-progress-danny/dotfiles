a4a056e9da50a6d8c112552cf827b6fb56e981aa
Danny Lowater <61439442+dannyknows@users.noreply.github.com>
2022-05-11 10:58:38 +1000

    fix: update deprecated lspconfig

--- .config/nvim/lua/lsp/lsp-installer.lua
+++ .config/nvim/lua/lsp/lsp-installer.lua
@@ -3,26 +3,46 @@ if not status_ok then
 	return
 end
 
+lsp_installer.setup()
+
+local lspconfig = require("lspconfig")
+
+lspconfig.sumneko_lua.setup({
+	init_options = { require("lsp.settings.sumneki_lua") },
+})
+
+lspconfig.jsonls.setup({
+	init_options = { require("lsp.settings.jsonls") },
+})
+
+lspconfig.pyright.setup({
+	init_options = { require("lsp.settings.pyright") },
+})
+
 -- Register a handler that will be called for all installed servers.
 -- Alternatively, you may also register handlers on specific server instances instead (see example below).
-lsp_installer.on_server_ready(function(server)
-	local opts = {
-		on_attach = require("lsp.handlers").on_attach,
-		capabilities = require("lsp.handlers").capabilities,
-	}
-	if server.name == "jsonls" then
-		local jsonls_opts = require("lsp.settings.jsonls")
-		opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
-	end
-	if server.name == "sumneko_lua" then
-		local sumneko_opts = require("lsp.settings.sumneki_lua")
-		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
-	end
-	if server.name == "pyright" then
-		local pyright_opts = require("lsp.settings.pyright")
-		opts = vim.tbl_deep_extend("force", pyright_opts, opts)
-	end
-	-- This setup() function is exactly the same as lspconfig's setup function.
-	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
-	server:setup(opts)
-end)
+-- lsp_installer.on_server_ready(function(server)
+-- 	local opts = {
+-- 		on_attach = require("lsp.handlers").on_attach,
+-- 		capabilities = require("lsp.handlers").capabilities,
+-- 	}
+-- 	if server.name == "jsonls" then
+-- 		local jsonls_opts = require("lsp.settings.jsonls")
+-- 		opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
+-- 	end
+-- 	if server.name == "sumneko_lua" then
+-- 		local sumneko_opts = require("lsp.settings.sumneki_lua")
+-- 		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
+-- 	end
+-- 	if server.name == "pyright" then
+-- 		local pyright_opts = require("lsp.settings.pyright")
+-- 		opts = vim.tbl_deep_extend("force", pyright_opts, opts)
+-- 	end
+-- 	-- This setup() function is exactly the same as lspconfig's setup function.
+-- 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
+-- 	server:setup(opts)
+-- end)
+
+-- lspconfig.tsserver.setup({
+-- 	init_options = { require("lsp.settings.tsserver") },
+-- })
