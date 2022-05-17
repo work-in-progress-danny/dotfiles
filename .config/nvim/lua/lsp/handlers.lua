74cbd435265e6b85e227425e071adf859380a63d
Danny Lowater <61439442+dannyknows@users.noreply.github.com>
2022-05-17 11:58:47 +1000

    chore: clean up old comments

--- .config/nvim/lua/lsp/handlers.lua
+++ .config/nvim/lua/lsp/handlers.lua
@@ -60,27 +60,6 @@ local function lsp_highlight_document(client)
 	end
 end
 
--- local function lsp_keymaps(bufnr)
--- 	local opts = { noremap = true, silent = true }
--- -- vim.api.nvim_buf_set_keymap(bufnr, "n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
--- vim.api.nvim_buf_set_keymap(bufnr, "n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
--- vim.api.nvim_buf_set_keymap(bufnr, "n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
--- vim.api.nvim_buf_set_keymap(bufnr, "n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts) -- get all references in codebase
---
--- vim.api.nvim_buf_set_keymap(bufnr, "n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
--- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>K", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
--- -- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>k", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
---
--- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts) -- rename all occurances under cursor
---
--- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>a", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts) -- code action under cursor
---
--- vim.api.nvim_buf_set_keymap(bufnr, "n", "[d", '<cmd>lua vim.diagnostic.goto_prev({ border = "rounded" })<CR>', opts)
--- vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
-
--- vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts) -- pops up a list of errors
--- vim.cmd([[ command! Format execute 'lua vim.lsp.buf.formatting()' ]])
--- end
 
 M.on_attach = function(client)
 	if
--- .config/nvim/lua/lsp/lsp-installer.lua
+++ .config/nvim/lua/lsp/lsp-installer.lua
@@ -40,29 +40,3 @@ lspconfig.tsserver.setup({
 	on_attach = on_attach,
 	capabilities = capabilities,
 })
-
--- Register a handler that will be called for all installed servers.
--- Alternatively, you may also register handlers on specific server instances instead (see example below).
--- lsp_installer.on_server_ready(function(server)
--- 	local opts = {
--- 	}
--- 	if server.name == "jsonls" then
--- 		local jsonls_opts = require("lsp.settings.jsonls")
--- 		opts = vim.tbl_deep_extend("force", jsonls_opts, opts)
--- 	end
--- 	if server.name == "sumneko_lua" then
--- 		local sumneko_opts = require("lsp.settings.sumneki_lua")
--- 		opts = vim.tbl_deep_extend("force", sumneko_opts, opts)
--- 	end
--- 	if server.name == "pyright" then
--- 		local pyright_opts = require("lsp.settings.pyright")
--- 		opts = vim.tbl_deep_extend("force", pyright_opts, opts)
--- 	end
--- 	-- This setup() function is exactly the same as lspconfig's setup function.
--- 	-- Refer to https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md
--- 	server:setup(opts)
--- end)
-
--- lspconfig.tsserver.setup({
--- 	init_options = { require("lsp.settings.tsserver") },
--- })
