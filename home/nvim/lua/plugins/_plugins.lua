28df681481b8cb8944b85e7aa915698e815ab545
Danny Lowater <61439442+Danny-Duck@users.noreply.github.com>
2022-06-20 09:19:39 +1000

    feat: use new grubox theme

--- home/nvim/lua/plugins/_plugins.lua
+++ home/nvim/lua/plugins/_plugins.lua
@@ -72,8 +72,9 @@ return packer.startup(function(use)
 	use("simrat39/rust-tools.nvim")
 
 	-- Colour Scheme
-	use("morhetz/gruvbox") -- My favourite colour scheme
-	use({ "catppuccin/nvim", as = "catppuccin" })
+	-- use("morhetz/gruvbox") -- My favourite colour scheme
+	-- use({ "catppuccin/nvim", as = "catppuccin" })
+	use("sainnhe/gruvbox-material")
 	use({ "p00f/nvim-ts-rainbow", requires = "vim-treesitter/nvim-treesitter" }) -- colours matching parenthesis
 
 	-- CMP Plugins
--- home/nvim/lua/user/colourscheme.lua
+++ home/nvim/lua/user/colourscheme.lua
@@ -1,6 +1,9 @@
-local colorscheme = "gruvbox"
+-- local colorscheme = "gruvbox"
 -- local colorscheme = "catppuccin"
+local colorscheme = "gruvbox-material"
 -- vim.g.catppuccin_flavour = "macchiato"
+
+vim.cmd("let g:gruvbox_material_background = 'hard'")
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
