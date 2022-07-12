9e70eb3a4bb5bfd1a184d8de47ed2a913c43cb5d
Danny Lowater <61439442+Danny-Duck@users.noreply.github.com>
2022-07-12 12:18:44 +1000

    style: clean up cmp with stylua ignore

--- home/nvim/lua/plugins/cmp.lua
+++ home/nvim/lua/plugins/cmp.lua
@@ -15,39 +15,41 @@ local check_backspace = function()
 	return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
 end
 
+
+-- stylua: ignore start 
 --   פּ ﯟ   some other good icons
 local kind_icons = {
-	copilot = "",
-	Text = "",
-	Method = "m",
-	Function = "",
-	Constructor = "",
-	Field = "",
-	Variable = "",
-	Class = "",
-	Interface = "",
-	Module = "",
-	Property = "",
-	Unit = "",
-	Value = "",
-	Enum = "",
-	Keyword = "",
-	Snippet = "",
-	Color = "",
-	File = "",
-	Reference = "",
-	Folder = "",
-	EnumMember = "",
-	Constant = "",
-	Struct = "",
-	Event = "",
-	Operator = "",
+	copilot       = "",
+	Text          = "",
+	Method        = "m",
+	Function      = "",
+	Constructor   = "",
+	Field         = "",
+	Variable      = "",
+	Class         = "",
+	Interface     = "",
+	Module        = "",
+	Property      = "",
+	Unit          = "",
+	Value         = "",
+	Enum          = "",
+	Keyword       = "",
+	Snippet       = "",
+	Color         = "",
+	File          = "",
+	Reference     = "",
+	Folder        = "",
+	EnumMember    = "",
+	Constant      = "",
+	Struct        = "",
+	Event         = "",
+	Operator      = "",
 	TypeParameter = "",
 }
+-- stylua: ignore end
 -- find more here: https://www.nerdfonts.com/cheat-sheet
 
 cmp.setup({
-
 	snippet = {
 		expand = function(args)
 			luasnip.lsp_expand(args.body) -- For `luasnip` users.
@@ -102,15 +104,17 @@ cmp.setup({
 			-- Kind icons
 			vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
 			-- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
+      -- stylua: ignore start
 			vim_item.menu = ({
-				copilot = "[Copilot]",
-				spell = "[Spell]",
+        copilot  = "[Copilot]",
+				spell    = "[Spell]",
 				nvim_lsp = "[LSP]",
-				luasnip = "[Snippet]",
-				buffer = "[File]",
-				path = "[Path]",
+				luasnip  = "[Snippet]",
+				buffer   = "[File]",
+				path     = "[Path]",
 			})[entry.source.name]
 			return vim_item
+			-- stylua: ignore end
 		end,
 	},
 	sources = {
