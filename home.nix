2a29456ab7b31b1257c3bcc42d128bf65b6b7ae8
Danny Lowater <61439442+Danny-Duck@users.noreply.github.com>
2022-07-30 13:57:43 +1000

    Refactor: home.nix

--- home.nix
+++ home.nix
@@ -1,87 +1,86 @@
-{ config, pkgs, ... }:
-
+{ pkgs }:
 {
-  # Home Manager needs a bit of information about you and the
-  # paths it should manage.
-  home =
-    {
-      # This value determines the Home Manager release that your
-      # configuration is compatible with. This helps avoid breakage
-      # when a new Home Manager release introduces backwards
-      # incompatible changes.
-      #
-      # You can update Home Manager without changing this value. See
-      # the Home Manager release notes for a list of state version
-      # changes in each release.
-      stateVersion = "22.05";
-      username = "danny.lowwater";
-      homeDirectory = "/Users/danny.lowater";
   imports = [
     home/git
     home/local.nix
   ];
 
-      packages = with pkgs; [
-        # Shell
-        zsh
-        zsh-autosuggestions
-        zsh-syntax-highlighting
-        zsh-history-substring-search
   home = {
+    # This value determines the Home Manager release that your
+    # configuration is compatible with. This helps avoid breakage
+    # when a new Home Manager release introduces backwards
+    # incompatible changes.
+    #
+    # You can update Home Manager without changing this value. See
+    # the Home Manager release notes for a list of state version
+    # changes in each release.
+    stateVersion = "22.05";
+    # these are defined in ~/.dotfiles/home/local.nix
+    # username = "";
+    # homeDirectory = "";
 
-        # Tools
-        starship
-        bat
-        comma # shopify's comma
-        direnv
-        exa
-        neovim
-        ripgrep # required by neovim's Telescope
-        fd
-        wget
-        tmux
-        htop-vim
+    packages = with pkgs; [
+      # Shell
+      zsh
+      zsh-autosuggestions
+      zsh-syntax-highlighting
+      zsh-history-substring-search
 
-        # Git 
-        git
-        gitui
-        gnupg
+      # Tools
+      starship
+      bat
+      comma # shopify's comma
+      direnv
+      exa
+      neovim
+      ripgrep # required by neovim's Telescope
+      fd
+      wget
+      tmux
+      htop-vim
 
-        # Languages
-        # yarn # yarn installs by default 16.14.2 and doesn't adhear to other installed versions see https://github.com/NixOS/nixpkgs/issues/145634
-        rust-analyzer
-        wrangler
-        # cargo installing these two manually as rust analyser can't find what it needs to run. see https://github.com/rust-lang/rust-analyzer/issues/9506
-        # rustc
-        fnm
+      # Git 
+      git
+      gitui
+      gnupg
 
-        # Neovim deps
-        cppcheck
-        stylua
-        rnix-lsp
-        statix
-        shellcheck
+      # Languages
+      # yarn # yarn installs by default 16.14.2 and doesn't adhear to other installed versions see https://github.com/NixOS/nixpkgs/issues/145634
+      rust-analyzer
+      wrangler
+      # cargo installing these two manually as rust analyser can't find what it needs to run. see https://github.com/rust-lang/rust-analyzer/issues/9506
+      # rustc
+      fnm
 
-        # Work
-        docker
-        postgresql
-        jdk11
-      ];
+      # Neovim deps
+      cppcheck
+      stylua
+      rnix-lsp
+      statix
+      shellcheck
+      ktlint # required to work with Kotlin
 
-      file = {
-        # ~
-        ".zshrc".source = ~/.dotfiles/home/zsh/.zshrc;
-        ".zprofile".source = ~/.dotfiles/home/zsh/.zprofile;
-        ".tmux.conf".source = ~/.dotfiles/home/.tmux.conf;
-        ".gitignore_global".source = ~/.dotfiles/home/.gitignore_global;
+      # Work
+      docker
+      postgresql
+      jdk11
+    ];
 
-        # ~/.config/ 
-        ".config/nvim".source = ~/.dotfiles/home/nvim;
-        ".config/nvim".recursive = true; # https://github.com/nix-community/home-manager/issues/2282
-        ".config/gitui".source = ~/.dotfiles/home/gitui;
-        ".config/alacritty.yml".source = ~/.dotfiles/home/alacritty.yml;
-        ".config/starship.toml".source = ~/.dotfiles/home/starship.toml;
+    file = {
+      # ~
+      ".zshrc".source = ~/.dotfiles/home/zsh/.zshrc;
+      ".zprofile".source = ~/.dotfiles/home/zsh/.zprofile;
+      ".tmux.conf".source = ~/.dotfiles/home/.tmux.conf;
+      ".ssh/config".source = ~/.dotfiles/home/ssh/config;
+
+      # ~/.config/
+      ".config/nvim" = {
+        source = ~/.dotfiles/home/nvim;
+        recursive = true; # https://github.com/nix-community/home-manager/issues/2282
       };
+      ".config/gitui".source = ~/.dotfiles/home/gitui;
+      ".config/alacritty.yml".source = ~/.dotfiles/home/alacritty.yml;
+      ".config/starship.toml".source = ~/.dotfiles/home/starship.toml;
     };
   };
 
