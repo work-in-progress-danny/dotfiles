03d401143ee967717e909a6ea64c77c5fea9bfcf
Danny Lowater <61439442+Danny-Duck@users.noreply.github.com>
2022-08-16 11:11:45 +1000

    Fix actually export shell variables

--- home/zsh/plugins.sh
+++ home/zsh/plugins.sh
@@ -3,11 +3,12 @@
 source "$HOME/.nix-profile/share/zsh-autosuggestions/zsh-autosuggestions.zsh"
 source "$HOME/.nix-profile/share/zsh-history-substring-search/zsh-history-substring-search.zsh"
 source "$HOME/.nix-profile/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh"
+
 # Auto suggest plugin settings
-ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#777777"
+export ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#777777"
 bindkey '^ ' autosuggest-accept
 
 # History substring search settings
 bindkey '^[[A' history-substring-search-up
 bindkey '^[[B' history-substring-search-down
-HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
+export HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
