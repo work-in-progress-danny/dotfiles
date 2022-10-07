89072e8a1f2d3b1af5511080a365257d67a92d91
Danny Lowater <61439442+Danny-Duck@users.noreply.github.com>
2022-10-08 08:47:49 +1100

    clean up spelling and move python export to environment.sh

--- home/zsh/environment.sh
+++ home/zsh/environment.sh
@@ -4,6 +4,7 @@ if [ "$TMUX" = "" ]; then tmux new-session -A -s main; fi
 
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 export SSH_AUTH_SOCK="$HOME/.1password/agent.sock"
+export PATH=~/Library/Python/3.8/bin:$PATH
 
 . "$HOME/.nix-profile/etc/profile.d/hm-session-vars.sh"
 eval "$(fnm env --use-on-cd)"
