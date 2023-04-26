529071c3c40e94da16f5a881ad1aee004a599634
Danny Lowater 🦆 <git@dannythomas.dev>
2023-04-26 14:32:47 +1000

    feat: remove vim-mode in zsh (change it to emacs)
    this was annoying and buggy whenever I pressed esc it would go into this border-line useless vim-mod e

--- home/zsh/preferences.sh
+++ home/zsh/preferences.sh
@@ -17,4 +17,6 @@ export STARSHIP_CONFIG="$HOME/.config/starship.toml"
 
 export EXA_COLORS="da=32" # da: timestamp
 
+bindkey -e # use emacs keybindings in zsh instead of vi
+
 autoload -U compinit && compinit
