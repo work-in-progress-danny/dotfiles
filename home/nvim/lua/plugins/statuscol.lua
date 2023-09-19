6dd87fa762dfec314bad68123a408352866dbbff
Danny Lowater 🫶 <git@dannythomas.dev>
2023-09-20 08:45:48 +1000

    feat: fix status column and fold icons

--- home/nvim/lua/plugins/statuscol.lua
+++ home/nvim/lua/plugins/statuscol.lua
@@ -1,56 +1,56 @@
 return {
   "luukvbaal/statuscol.nvim",
   config = function()
-    --[[ local builtin = require("statuscol.builtin") ]]
-    --[[ local cfg = { ]]
-    --[[   setopt = true, -- Whether to set the 'statuscolumn' option, may be set to false for those who ]]
-    --[[   -- want to use the click handlers in their own 'statuscolumn': _G.Sc[SFL]a(). ]]
-    --[[   -- Although I recommend just using the segments field below to build your ]]
-    --[[   -- statuscolumn to benefit from the performance optimizations in this plugin. ]]
-    --[[   -- builtin.lnumfunc number string options ]]
-    --[[   thousands = false, -- or line number thousands separator string ("." / ",") ]]
-    --[[   relculright = false, -- whether to right-align the cursor line number with 'relativenumber' set ]]
-    --[[   -- Builtin 'statuscolumn' options ]]
-    --[[   ft_ignore = nil,  -- lua table with filetypes for which 'statuscolumn' will be unset ]]
-    --[[   bt_ignore = nil,  -- lua table with 'buftype' values for which 'statuscolumn' will be unset ]]
-    --[[   -- Default segments (fold -> sign -> line number + separator), explained below ]]
-    --[[   foldfunc = "builtin", ]]
-    --[[   segments = { ]]
-    --[[     { text = { "%C" }, click = "v:lua.ScFa" }, ]]
-    --[[     { text = { "%s" }, click = "v:lua.ScSa" }, ]]
-    --[[     { ]]
-    --[[       text = { builtin.lnumfunc, " " }, ]]
-    --[[       condition = { true, builtin.not_empty }, ]]
-    --[[       click = "v:lua.ScLa", ]]
-    --[[     }, ]]
-    --[[   }, ]]
-    --[[   clickmod = "c", -- modifier used for certain actions in the builtin clickhandlers: ]]
-    --[[   -- "a" for Alt, "c" for Ctrl and "m" for Meta. ]]
-    --[[   clickhandlers = { -- builtin click handlers ]]
-    --[[     Lnum = builtin.lnum_click, ]]
-    --[[     FoldClose = builtin.foldclose_click, ]]
-    --[[     FoldOpen = builtin.foldopen_click, ]]
-    --[[     FoldOther = builtin.foldother_click, ]]
-    --[[     DapBreakpointRejected = builtin.toggle_breakpoint, ]]
-    --[[     DapBreakpoint = builtin.toggle_breakpoint, ]]
-    --[[     DapBreakpointCondition = builtin.toggle_breakpoint, ]]
-    --[[     DiagnosticSignError = builtin.diagnostic_click, ]]
-    --[[     DiagnosticSignHint = builtin.diagnostic_click, ]]
-    --[[     DiagnosticSignInfo = builtin.diagnostic_click, ]]
-    --[[     DiagnosticSignWarn = builtin.diagnostic_click, ]]
-    --[[     GitSignsTopdelete = builtin.gitsigns_click, ]]
-    --[[     GitSignsUntracked = builtin.gitsigns_click, ]]
-    --[[     GitSignsAdd = builtin.gitsigns_click, ]]
-    --[[     GitSignsChange = builtin.gitsigns_click, ]]
-    --[[     GitSignsChangedelete = builtin.gitsigns_click, ]]
-    --[[     GitSignsDelete = builtin.gitsigns_click, ]]
-    --[[     gitsigns_extmark_signs_ = builtin.gitsigns_click, ]]
-    --[[   }, ]]
-    --[[ } ]]
-    --[[ require("statuscol").setup(cfg) ]]
+    local builtin = require("statuscol.builtin")
+
+    vim.o.fillchars = [[eob: ,fold: ,foldopen:,foldsep: ,foldclose:]]
+    vim.o.foldcolumn = "1"
+    vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
+    vim.o.foldlevelstart = 99
+    vim.o.foldenable = true
+    --[[ vim.cmd([[: highlight FoldColumn guibg=#1e222a guifg=]]
+
+    Get_and_set_gruvbox_highlight_group("GruvboxBg0", "guifg", "FoldColumn", "guibg")
+    Get_and_set_gruvbox_highlight_group("GruvboxFg1", "guifg", "FoldColumn", "guifg")
+
     require("statuscol").setup({
-      foldfunc = "builtin",
-      setopt = true,
+
+      segments = {
+        {
+          text = { builtin.foldfunc },
+          click = "v:lua.ScFa",
+          auto = true,
+          condition = { builtin.not_empty }, -- table of booleans or functions returning a boolean
+          hl = "FoldColumn",            -- %# highlight group label, applies to each text element
+          bg = "Normal",                -- %* highlight group label, applies to the background
+          sign = {                      -- table of fields that configure a sign segment
+            -- at least one of "name", "text", and "namespace" is required
+            -- legacy signs are matched against the defined sign name e.g. "DiagnosticSignError"
+            -- extmark signs can be matched agains either the namespace or the sign text itself
+            name = { ".*" }, -- table of lua patterns to match the sign name against
+            text = { ".*" }, -- table of lua patterns to match the extmark sign text against
+            namespace = { ".*" }, -- table of lua patterns to match the extmark sign namespace against
+            -- below values list the default when omitted:
+            maxwidth = 1,   -- maximum number of signs that will be displayed in this segment
+            colwidth = 2,   -- number of display cells per sign in this segment
+            auto = false,   -- when true, the segment will not be drawn if no signs matching
+            -- the pattern are currently placed in the buffer.
+            wrap = false,   -- when true, signs in this segment will also be drawn on the
+            -- virtual or wrapped part of a line (when v:virtnum != 0).
+            fillchar = " ", -- character used to fill a segment with less signs than maxwidth
+            fillcharhl = nil, -- highlight group used for fillchar (SignColumn/CursorLineSign if omitted)
+          },
+        },
+        {
+          sign = { name = { "Diagnostic" }, maxwidth = 1 },
+          click = "v:lua.ScSa",
+        },
+        { text = { builtin.lnumfunc }, click = "v:lua.ScLa" },
+        {
+          sign = { name = { ".*" }, maxwidth = 2, colwidth = 1, wrap = true, auto = true },
+          click = "v:lua.ScSa",
+        },
+      },
     })
   end,
 }
