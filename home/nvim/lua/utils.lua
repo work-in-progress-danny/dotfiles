b80d5046fc48cdb86a6e2186871093dc2d039e65
Danny Lowater 🫶 <git@dannythomas.dev>
2023-09-20 08:44:17 +1000

    feat: create gruvbox colour scheme highlight helper

--- home/nvim/lua/utils.lua
+++ home/nvim/lua/utils.lua
@@ -1,3 +1,6 @@
+-- Desc: Utility functions for use in other lua files
+
+-- Try to require a package, gracefully fail and print out some debug info
 function TryRequire(package_name)
   local status_ok, package = pcall(require, package_name)
   local file_path = debug.getinfo(2, "S").source
@@ -11,6 +14,31 @@ function TryRequire(package_name)
   return package
 end
 
+-- Shorthand for vim.cmd
 function Cmd(command_body)
   return "<cmd>" .. command_body .. "<cr>"
+end
+
+-- A nice way to set highlight groups from Gruvbox's highlight groups
+-- example:
+-- Get_and_set_gruvbox_highlight_group("GruvboxBg0", "guifg", "FoldColumn", "guibg")
+-- You can find the Gruvbox highlight groups by running :hi Gruvbox<tab>
+function Get_and_set_gruvbox_highlight_group(
+  gruvbox_highlight,
+  gruvbox_highlight_attribute,
+  target_highlight_group,
+  target_highlight_attribute
+)
+  -- Get the attributes of the source highlight group
+  local highlight_info = vim.api.nvim_exec("highlight " .. gruvbox_highlight, true)
+
+  -- Extract the desired attribute (like guibg, guifg, ctermfg, etc.)
+  local colour = string.match(highlight_info, gruvbox_highlight_attribute .. "=(%S+)")
+  if not colour then
+    print("Unable to find " .. gruvbox_highlight_attribute .. " in " .. gruvbox_highlight)
+    return
+  else
+    -- Set the attribute for the target highlight group
+    vim.cmd("highlight " .. target_highlight_group .. " " .. target_highlight_attribute .. "=" .. colour)
+  end
 end
