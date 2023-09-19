8a20fcc02d15497bcb78720e593890aeb9b44c27
Danny Lowater 🫶 <git@dannythomas.dev>
2023-09-20 08:47:32 +1000

    chore: minor cleaning and formatting udpdates

--- home/nvim/lua/plugins/null-ls.lua
+++ home/nvim/lua/plugins/null-ls.lua
@@ -1,87 +1,81 @@
 -- https://www.youtube.com/watch?v=e3xxkEbhG0o
 
 return {
-	"jose-elias-alvarez/null-ls.nvim",
-	dependencies = {
-		"jay-babu/mason-null-ls.nvim",
-		{ "j-hui/fidget.nvim", tag = "legacy" }, -- nice little notifier in the corner communicating the current state of Null-ls
-	},
-	config = function()
-		local null_ls = TryRequire("null-ls")
-		local fidget = TryRequire("fidget")
+  "jose-elias-alvarez/null-ls.nvim",
+  dependencies = {
+    "jay-babu/mason-null-ls.nvim",
+    { "j-hui/fidget.nvim", tag = "legacy" }, -- nice little notifier in the corner communicating the current state of Null-ls
+  },
+  config = function()
+    local null_ls = require("null-ls")
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+    local formatting = null_ls.builtins.formatting
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+    local diagnostics = null_ls.builtins.diagnostics
+    -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+    local code_actions = null_ls.builtins.code_actions
 
-		if not null_ls or not fidget then
-			return
-		end
+    null_ls.setup({
+      --[[ debug = true, ]]
+      sources = {
+        -- Javascript / Typescript
+        diagnostics.eslint_d,
+        code_actions.eslint_d,
+        formatting.eslint_d,
 
-		-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-		local formatting = null_ls.builtins.formatting
-		-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-		local diagnostics = null_ls.builtins.diagnostics
-		-- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-		local code_actions = null_ls.builtins.code_actions
+        formatting.prettierd.with({
+          extra_filetypes = {
+            "TOML",
+            "astro",
+            "css",
+            "scss",
+            "less",
+            "html",
+            "json",
+            "yaml",
+            "markdown",
+            "graphql",
+          },
+        }),
 
-		null_ls.setup({
-			--[[ debug = true, ]]
-			sources = {
-				-- Javascript / Typescript
-				diagnostics.eslint_d,
-				code_actions.eslint_d,
-				formatting.eslint_d,
+        -- Lua
+        formatting.stylua,
+        diagnostics.luacheck.with({
+          args = { "--no-color" },
+        }),
 
-				formatting.prettierd.with({
-					extra_filetypes = {
-						"TOML",
-						"astro",
-						"css",
-						"scss",
-						"less",
-						"html",
-						"json",
-						"yaml",
-						"markdown",
-						"graphql",
-					},
-				}),
+        -- Nix
+        diagnostics.statix,
+        code_actions.statix,
 
-				-- Lua
-				formatting.stylua,
-				diagnostics.luacheck.with({
-					args = { "--no-color" },
-				}),
+        -- Bash
+        diagnostics.shellcheck,
 
-				-- Nix
-				diagnostics.statix,
-				code_actions.statix,
+        -- C
+        diagnostics.cppcheck,
+        -- formatting is done by clangd (lsp-config)
 
-				-- Bash
-				diagnostics.shellcheck,
+        -- Rust (rust-analyzer) is handled by rust-tools.nvim
+        formatting.rustfmt.with({
+          extra_args = function(params)
+            local Path = require("plenary.path")
+            local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
 
-				-- C
-				diagnostics.cppcheck,
-				-- formatting is done by clangd (lsp-config)
+            if cargo_toml:exists() and cargo_toml:is_file() then
+              for _, line in ipairs(cargo_toml:readlines()) do
+                local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
+                if edition then
+                  return { "--edition=" .. edition }
+                end
+              end
+            end
+            -- default edition when we don't find `Cargo.toml` or the `edition` in it.
+            return { "--edition=2021" }
+          end,
+        }),
+      },
+    })
 
-				-- Rust (rust-analyzer) is handled by rust-tools.nvim
-				formatting.rustfmt.with({
-					extra_args = function(params)
-						local Path = require("plenary.path")
-						local cargo_toml = Path:new(params.root .. "/" .. "Cargo.toml")
-
-						if cargo_toml:exists() and cargo_toml:is_file() then
-							for _, line in ipairs(cargo_toml:readlines()) do
-								local edition = line:match([[^edition%s*=%s*%"(%d+)%"]])
-								if edition then
-									return { "--edition=" .. edition }
-								end
-							end
-						end
-						-- default edition when we don't find `Cargo.toml` or the `edition` in it.
-						return { "--edition=2021" }
-					end,
-				}),
-			},
-		})
-
-		fidget.setup() -- provide a UI for nvim-lsp's progress handler
-	end,
+    require("fidget").setup() -- provide a UI for nvim-lsp's progress handler
+  end,
 }
--- home/nvim/lua/plugins/treesitter.lua
+++ home/nvim/lua/plugins/treesitter.lua
@@ -1,54 +1,56 @@
 -- https://www.youtube.com/watch?v=hkxPa5w3bZ0&list=PLhoH5vyxr6Qq41NFL4GvhFp-WLd5xzIzZ&index=10
 return {
-  "nvim-treesitter/nvim-treesitter",
-  dependencies = {
-    "windwp/nvim-ts-autotag",
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    "p00f/nvim-ts-rainbow",
-  },
-  build = ":TSUpdate",
-  config = {
-    ensure_installed = {
-      "lua",
-      "json",
-      "typescript",
-      "javascript",
-    },                                 -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    sync_install = false,              -- install languages synchronously (only applied to `ensure_installed`)
-    ignore_install = { "t32", "smali" }, -- List of parsers to ignore installing
-    highlight = {
-      enable = true,                   -- false will disable the whole extension
-      disable = { "" },                -- list of language that will be disabled
-      additional_vim_regex_highlighting = true,
-    },
-    indent = {
-      enable = true,
-      -- disable = { "yaml" }
-    },
-    playground = {
-      enable = true,
-    },
-    autopairs = {
-      enable = true,
-    },
-    rainbow = {
-      enable = true,
-      extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-      max_file_lines = nil, -- Do not enable for files with more than n lines, int
-      -- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
-      colors = {
-        "#E06C75",
-        "#E5C07B",
-        "#98C379",
-        "#56B6C2",
-        "#61AFEF",
-        "#C678DD",
-      }, -- table of hex strings, this matches the colours set in indent-blankline.lua
-      -- termcolors = {} -- table of colour name strings
-    },
-    context_commentstring = {
-      enable = true,
-      enable_autocmd = false,
-    },
-  },
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		"windwp/nvim-ts-autotag",
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		"p00f/nvim-ts-rainbow",
+	},
+	build = ":TSUpdate",
+	config = {
+		ensure_installed = {
+			"lua",
+			"json",
+			"typescript",
+			"javascript",
+			"jsx",
+			"tsx",
+		}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+		sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
+		ignore_install = { "t32", "smali" }, -- List of parsers to ignore installing
+		highlight = {
+			enable = true, -- false will disable the whole extension
+			disable = { "" }, -- list of language that will be disabled
+			additional_vim_regex_highlighting = true,
+		},
+		indent = {
+			enable = true,
+			-- disable = { "yaml" }
+		},
+		playground = {
+			enable = true,
+		},
+		autopairs = {
+			enable = true,
+		},
+		rainbow = {
+			enable = true,
+			extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
+			max_file_lines = nil, -- Do not enable for files with more than n lines, int
+			-- disable = { "jsx", "cpp" }, list of languages you want to disable the plugin for
+			colors = {
+				"#E06C75",
+				"#E5C07B",
+				"#98C379",
+				"#56B6C2",
+				"#61AFEF",
+				"#C678DD",
+			}, -- table of hex strings, this matches the colours set in indent-blankline.lua
+			-- termcolors = {} -- table of colour name strings
+		},
+		context_commentstring = {
+			enable = true,
+			enable_autocmd = false,
+		},
+	},
 }
