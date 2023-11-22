c5fbf45baf3c3475e2aec210b394c3e929c63fc6
Danny Lowater 🫶 <git@dannythomas.dev>
2023-11-23 09:57:50 +1100

    fix: nvim-tree

--- home/nvim/lua/plugins/nvim-tree.lua
+++ home/nvim/lua/plugins/nvim-tree.lua
@@ -44,7 +44,7 @@ return {
 	dependencies = {
 		"nvim-tree/nvim-web-devicons",
 	},
-	opts = function()
+	config = function()
 		local nvim_tree = require("nvim-tree")
 		nvim_tree.setup({
 			renderer = {
