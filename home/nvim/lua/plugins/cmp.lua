b4e72ef466ddedb1073f6f9cc5c8abcd5f3e4bc8
Danny Lowater 🫶 <git@dannythomas.dev>
2023-11-23 09:58:06 +1100

    fix: formatting

--- home/nvim/lua/plugins/cmp.lua
+++ home/nvim/lua/plugins/cmp.lua
@@ -1,30 +1,30 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-buffer",     -- buffer completions
-    "hrsh7th/cmp-path",       -- path completions
-    "hrsh7th/cmp-cmdline",    -- cmdline completions
-    "saadparwaiz1/cmp_luasnip", -- snippet completions
-    "hrsh7th/cmp-nvim-lsp",   -- LSP completions
-    "f3fora/cmp-spell",       -- spell completions
-    "windwp/nvim-autopairs",
-    "zbirenbaum/copilot.lua",
-  },
-  config = function()
-    local cmp = TryRequire("cmp")
-    local luasnip = TryRequire("luasnip")
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer", -- buffer completions
+		"hrsh7th/cmp-path", -- path completions
+		"hrsh7th/cmp-cmdline", -- cmdline completions
+		"saadparwaiz1/cmp_luasnip", -- snippet completions
+		"hrsh7th/cmp-nvim-lsp", -- LSP completions
+		"f3fora/cmp-spell", -- spell completions
+		"windwp/nvim-autopairs",
+		"zbirenbaum/copilot.lua",
+	},
+	config = function()
+		local cmp = TryRequire("cmp")
+		local luasnip = TryRequire("luasnip")
 
-    if not cmp or not luasnip then
-      return
-    end
+		if not cmp or not luasnip then
+			return
+		end
 
-    require("luasnip/loaders/from_vscode").lazy_load()
+		require("luasnip/loaders/from_vscode").lazy_load()
 
-    local check_backspace = function()
-      local col = vim.fn.col(".") - 1
-      return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
-    end
+		local check_backspace = function()
+			local col = vim.fn.col(".") - 1
+			return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+		end
 
     -- stylua: ignore start
     --   פּ ﯟ   some other good icons
@@ -57,62 +57,62 @@ return {
       Operator      = "",
       TypeParameter = "",
     }
-    -- stylua: ignore end
+		-- stylua: ignore end
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body) -- For `luasnip` users.
-        end,
-      },
-      mapping = {
-        -- ["<C-k>"] = cmp.mapping.select_prev_item,
-        -- ["<C-j>"] = cmp.mapping.select_next_item(),
-        -- ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
-        -- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
-        -- ["<C-e>"] = cmp.mapping({
-        -- 	i = cmp.mapping.abort(),
-        -- 	c = cmp.mapping.close(),
-        -- }),
-        -- Accept currently selected item. If none selected, `select` first item.
-        -- Set `select` to `false` to only confirm explicitly selected items.
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-k>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
-        ["<C-j>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.expandable() then
-            luasnip.expand()
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif check_backspace() then
-            fallback()
-          else
-            fallback()
-          end
-        end, {
-          "i",
-          "s",
-        }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, {
-          "i",
-          "s",
-        }),
-      },
-      formatting = {
-        fields = { "kind", "abbr", "menu" },
-        format = function(entry, vim_item)
-          -- Kind icons
-          vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
+			mapping = {
+				-- ["<C-k>"] = cmp.mapping.select_prev_item,
+				-- ["<C-j>"] = cmp.mapping.select_next_item(),
+				-- ["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
+				-- ["<C-y>"] = cmp.config.disable, -- Specify `cmp.config.disable` if you want to remove the default `<C-y>` mapping.
+				-- ["<C-e>"] = cmp.mapping({
+				-- 	i = cmp.mapping.abort(),
+				-- 	c = cmp.mapping.close(),
+				-- }),
+				-- Accept currently selected item. If none selected, `select` first item.
+				-- Set `select` to `false` to only confirm explicitly selected items.
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+				["<C-k>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+				["<C-j>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expandable() then
+						luasnip.expand()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					elseif check_backspace() then
+						fallback()
+					else
+						fallback()
+					end
+				end, {
+					"i",
+					"s",
+				}),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, {
+					"i",
+					"s",
+				}),
+			},
+			formatting = {
+				fields = { "kind", "abbr", "menu" },
+				format = function(entry, vim_item)
+					-- Kind icons
+					vim_item.kind = string.format("%s", kind_icons[vim_item.kind])
           -- vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatonates the icons with the name of the item kind
           -- stylua: ignore start
           vim_item.menu = ({
@@ -124,30 +124,30 @@ return {
             path     = "[Path]",
           })[entry.source.name]
           return vim_item
-          -- stylua: ignore end
-        end,
-      },
-      sources = {
-        { name = "copilot" },
-        { name = "nvim_lsp" },
-        { name = "spell",   keyword_length = 4 },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "path" },
-      },
-      confirm_opts = {
-        behavior = cmp.ConfirmBehavior.Replace,
-        select = false,
-      },
-      window = {
-        documentation = {
-          border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
-        },
-      },
-      experimental = {
-        ghost_text = true,
-        native_menu = false,
-      },
-    })
-  end,
+					-- stylua: ignore end
+				end,
+			},
+			sources = {
+				{ name = "copilot" },
+				{ name = "nvim_lsp" },
+				{ name = "spell", keyword_length = 4 },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+			},
+			confirm_opts = {
+				behavior = cmp.ConfirmBehavior.Replace,
+				select = false,
+			},
+			window = {
+				documentation = {
+					border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+				},
+			},
+			experimental = {
+				ghost_text = true,
+				native_menu = false,
+			},
+		})
+	end,
 }
