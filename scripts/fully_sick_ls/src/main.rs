99598d13679f0f5d990fd8025a0110ee4c468d00
Danny Lowater 🫶 <git@dannythomas.dev>
2023-11-24 09:36:02 +1100

    feat: update fully_sick_ls to handle incorrect props gracefully

--- scripts/fully_sick_ls/src/main.rs
+++ scripts/fully_sick_ls/src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
 use std::{path::Path, process::Command};
+use colored::*;
 
 // A fully sick implementation of exa, which is a fully sick implementation of UNIX's ls command
 // https://github.com/ogham/exa
@@ -14,10 +15,40 @@ struct Args {
     depth: String,
 }
 
+fn is_valid_directory<P: AsRef<Path>>(path: P) -> bool {
+    path.as_ref().is_dir()
+}
+
+fn is_valid_integer(s: &str) -> bool {
+    s.parse::<i32>().is_ok()
+}
+
+fn print_usage() {
+    println!("{} {} {} {} {}",
+             "Usage:".bold(),
+             "fully_sick_ls".green(),
+             "<path>".yellow(),
+             "optional:",
+             "<depth>".blue(),
+             );
+}
+
 fn main() {
     let Args { path, depth } = Args::parse();
     let full_path = Path::new(&path);
     let full_depth = format!("--level={}", depth);
+
+    if !is_valid_directory(&full_path) {
+        println!("\"{}\" is an invalid directory", full_path.display());
+        print_usage();
+        return ();
+    }
+    
+    if !is_valid_integer(&depth) {
+        println!("\"{}\" is not a number", depth);
+        print_usage();
+        return ();
+    }
 
     let args = [
         &full_depth,
