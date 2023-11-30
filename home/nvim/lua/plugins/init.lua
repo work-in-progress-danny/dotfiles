01dd6ed1f58e2f88f2d3d915a71c94b312b9b4ea
Danny Lowater 🫶 <git@dannythomas.dev>
2023-11-30 12:21:55 +1100

    feat: switch vim-surround to nvim-surround

--- home/nvim/lua/plugins/init.lua
+++ home/nvim/lua/plugins/init.lua
@@ -19,12 +19,10 @@ return {
 	"nvim-lua/popup.nvim", -- An implementation of the Popup API from vim in Neovim
 	"nvim-lua/plenary.nvim", -- Useful lua functions used with lots of plugins
 	{ "lilydjwg/colorizer", event = "BufRead" }, -- highlight colour codes with their colour e.g #282 #718
-	{ "moll/vim-bbye" }, -- delete buffers without closing windows
+	"moll/vim-bbye", -- delete buffers without closing windows
 
 	-- Language support. Null-ls and Neovim's LSP handles almost everything
 	{ "simrat39/rust-tools.nvim", event = "BufRead" },
-
-	-- Snippets
 
 	-- LSP ( Language Server Protocol support )
 	{ "williamboman/mason-lspconfig.nvim", event = "BufRead" },
@@ -32,6 +30,6 @@ return {
 
 	-- Text Manipulation
 	{ "AndrewRadev/splitjoin.vim", event = "BufRead" }, -- split a single line of code into multiple and multiple into single
-	{ "tpope/vim-surround", event = "BufRead" }, -- Provides an interface to wrap a given block with a symbol
+	{ "kylechui/nvim-surround", dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" }, event = "BufRead" },
 	{ "godlygeek/tabular", event = "BufRead" }, -- Takes a regular expression and aligns a block of text depending on that
 }
