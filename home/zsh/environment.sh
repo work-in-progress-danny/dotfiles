fd90fe64df116c008eae811a4a69392cd24ff32f
Danny Lowater 😴 <git@dannythomas.dev>
2024-01-29 09:58:22 +1100

    chore: clean up here and there

--- home/zsh/environment.sh
+++ home/zsh/environment.sh
@@ -2,15 +2,8 @@
 
 if [ "$TMUX" = "" ]; then tmux new-session -A -s main; fi
 
-# export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-export PATH=~/Library/Python/3.8/bin:$PATH
+eval "$(starship init zsh)"
 
 # eval "$(fnm env --use-on-cd)"
-# eval "$(direnv hook zsh)"
-
-eval "$(/opt/homebrew/bin/starship init zsh)"
-
-export PATH=/Users/danny.lowater/Library/Android/sdk/platform-tools:$PATH
 
 source "$HOME/.cargo/env"
-# curl -s -H \"Accept: text/plain\" https://icanhazdadjoke.com/ | cowsay
