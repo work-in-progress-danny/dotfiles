4de4343d0857e88082e2203bdf42275ca72fe2be
Danny Lowater 🫶 <git@dannythomas.dev>
2024-03-27 10:59:38 +1100

    feat: add taplo toml formatter

--- config/nvim/lua/plugins/conform.lua
+++ config/nvim/lua/plugins/conform.lua
@@ -3,20 +3,15 @@ return {
 	event = { "BufWritePre" },
 	cmd = { "ConformInfo" },
 	opts = {
-		-- Set up format-on-save
 		format_on_save = { timeout_ms = 10000, lsp_fallback = true },
-		-- Define your formatters
 		formatters_by_ft = {
 			lua = { "stylua" },
 			rust = { "rustfmt" },
-			-- ESLINT FUCKING SUCKS AS A FORMATTER
-			-- TIMES OUT AFTER 10 SECONDS ON A 75 LINE FILE
-			--[[ javascript = { "eslint_d" }, ]]
-			--[[ javascriptreact = { "eslint_d" }, ]]
-			--[[ typescript = { "eslint_d" }, ]]
-			--[[ typescriptreact = { "eslint_d" }, ]]
+			typescript = { "biome" },
+			javascript = { "biome" },
+			javascriptreact = { "biome" },
+			typescriptreact = { "biome" },
+			toml = { "taplo" },
 		},
-		-- Customize formatters
-		formatters = {},
 	},
 }
