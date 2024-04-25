71fad4e33c9af4c0874d1de25b3e28b5e1588001
Danny Lowater 🫶 dotfiles only <git@dannythomas.dev>
2024-04-25 12:09:01 +1000

    fix: config is deprecated, using opts instead

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -2,7 +2,7 @@ return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
 	dependencies = { "nvim-lua/plenary.nvim" },
-	config = {
+	opts = {
 		settings = {
 			save_on_toggle = true,
 			sync_on_ui_close = true,
