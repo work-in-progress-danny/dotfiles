60b597d0162c48b8c6b89d0cd1d4de3ce537ea84
Danny Lowater 🫶 dotfiles only <git@dannythomas.dev>
2024-05-24 14:45:46 +1000

    fix: remove vale

--- config/nvim/lua/plugins/nvim-lint.lua
+++ config/nvim/lua/plugins/nvim-lint.lua
@@ -3,7 +3,6 @@ return {
 	config = function()
 		local lint = require("lint")
 		lint.linters_by_ft = {
-			markdown = { "vale" },
 			javascript = { "eslint_d" },
 			typescript = { "eslint_d" },
 			javascriptreact = { "eslint_d" },
