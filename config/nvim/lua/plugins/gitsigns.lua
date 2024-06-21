70967ac2a6335d0bbd48df729a2b7ca3606d448a
Danny Lowater 🫶 dotfiles only <git@dannythomas.dev>
2024-06-21 15:57:21 +1000

    chore: update gitsigns to drop deprecated fields

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -2,9 +2,6 @@
 return {
 	"lewis6991/gitsigns.nvim",
 	event = "BufRead",
-	dependencies = {
-		--[[ "nvim-lua/plenary.nvim", ]]
-	},
 	config = function()
 		Get_and_set_gruvbox_highlight_group("GruvboxGreen", "guifg", "GitSignsAdd", "guifg")
 		Get_and_set_gruvbox_highlight_group("GruvboxRed", "guifg", "GitSignsDelete", "guifg")
@@ -13,31 +10,11 @@ return {
 
 		require("gitsigns").setup({
 			signs = {
-				add = { hl = "GitSignsAdd", text = "┃", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-				change = {
-					hl = "GitSignsChange",
-					text = "┃",
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
-				},
-				delete = {
-					hl = "GitSignsDelete",
-					text = "┃",
-					numhl = "GitSignsDeleteNr",
-					linehl = "GitSignsDeleteLn",
-				},
-				topdelete = {
-					hl = "GitSignsDelete",
-					text = "┃",
-					numhl = "GitSignsDeleteNr",
-					linehl = "GitSignsDeleteLn",
-				},
-				changedelete = {
-					hl = "GitSignsChange",
-					text = "┃",
-					numhl = "GitSignsChangeNr",
-					linehl = "GitSignsChangeLn",
-				},
+				add = { text = "┃" },
+				change = { text = "┃" },
+				delete = { text = "┃" },
+				topdelete = { text = "┃" },
+				changedelete = { text = "┃" },
 			},
 			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
 			numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
@@ -57,9 +34,6 @@ return {
 				abbrev_sha = true,
 			},
 			current_line_blame_formatter = " <author>, <author_time> - <summary> #<abbrev_sha>",
-			current_line_blame_formatter_opts = {
-				relative_time = false,
-			},
 			sign_priority = 6,
 			update_debounce = 100,
 			status_formatter = nil, -- Use default
@@ -71,9 +45,6 @@ return {
 				relative = "cursor",
 				row = 0,
 				col = 1,
-			},
-			yadm = {
-				enable = false,
 			},
 		})
 	end,
