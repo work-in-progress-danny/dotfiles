e094798e1e7227ef4d91ce05b1391147fba6e3ca
Danny Lowater 🫶 <git@dannythomas.dev>
2024-09-08 15:06:37 +0200

    chore: update whichkey configs to follow v3 api

--- config/nvim/lua/plugins/whichkey.lua
+++ config/nvim/lua/plugins/whichkey.lua
@@ -1,131 +1,204 @@
+local leader = "<leader>"
+
 return {
 	"folke/which-key.nvim", -- Whichkey ( Keybinding Prompt on leader-key down )
-	config = function()
-		local which_key = require("which-key")
-
-		local setup_preferences = {
-			ignore_missing = true,
-			window = {
-				border = "rounded", -- none, single, double, shadow
-				position = "bottom", -- bottom, top
-				margin = { 1, 0, 1, 0 }, -- extra window margin [top, right, bottom, left]
-				padding = { 2, 2, 2, 2 }, -- extra window padding [top, right, bottom, left]
-				winblend = 0,
+	dependencies = { "echasnovski/mini.icons", version = false },
+	event = "VeryLazy",
+	opts = {
+		preset = "helix",
+		plugins = {
+			presets = {
+				operators = false, -- adds help for operators like d, y, ...
+				motions = false, -- adds help for motions
+				text_objects = false, -- help for text objects triggered after entering an operator
+				windows = false, -- default bindings on <c-w>
+				nav = false, -- misc bindings to work with windows
+				z = false, -- bindings for folds, spelling and others prefixed with z
+				g = false, -- bindings for prefixed with g
 			},
-			layout = {
-				height = { min = 4, max = 25 }, -- min and max height of the columns
-				width = { min = 20, max = 50 }, -- min and max width of the columns
-				spacing = 3, -- spacing between columns
-				align = "left", -- align columns left, center or right
-			},
-			icons = {
-				breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-				separator = "", -- symbol used between a key and it's label
-				group = "+", -- symbol prepended to a group
-			},
-		}
-
-		local normal_mode_opts = {
-			mode = "n", -- NORMAL mode
-			prefix = " ",
-			buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-			silent = true, -- use `silent` when creating keymaps
-			noremap = true, -- use `noremap` when creating keymaps
-			nowait = true, -- use `nowait` when creating keymaps
-		}
-
-		local normal_mode_mappings = {
-			-- Code Management
-			n = { Cmd("lua vim.diagnostic.goto_next({ border = 'rounded' })"), "Jump to next error" },
-			N = { Cmd("lua vim.diagnostic.goto_prev({ border = 'rounded' })"), "Jump to previous error" },
-			k = {
-				name = "signature help",
-				-- TODO add a script check whether there is diagnostics to show if not display variable signature
-				K = { Cmd("lua vim.lsp.buf.hover()"), "Show hover" },
-				h = { Cmd("lua vim.lsp.buf.signature_help()"), "Signature help" },
-				k = { Cmd("lua vim.diagnostic.open_float()"), "Show diagnostics" },
-			},
-			r = { Cmd("lua vim.lsp.buf.rename()"), "Rename all instances" },
-			a = { Cmd("lua vim.lsp.buf.code_action()"), "Display code action" },
-
-			-- Git diff
-			d = {
-				name = "Git diff",
-				f = { Cmd("Telescope git_status hidden=true"), "files" },
-				o = { Cmd("DiffviewOpen"), "open diff view" },
-				c = { Cmd("DiffviewClose"), "close diff view" },
-				p = { Cmd("diffput"), "put diff back" },
-			},
-
-			-- Telescope
-			f = { Cmd("Telescope find_files hidden=true"), "Find files" },
-			G = { Cmd('lua require("telescope").extensions.live_grep_args.live_grep_args()'), "Grep text" },
-			b = { Cmd("Telescope buffers"), "Buffers" },
-			s = { Cmd("Telescope current_buffer_fuzzy_find"), "Search current buffer" },
-			R = { Cmd("Telescope resume"), "Resume last Telescope instance" },
-			T = {
-				name = "Telescope",
-				t = { Cmd("Telescope"), "Toggle Telescope" },
-				c = { Cmd("Telescope command_history"), "Command History" },
-				h = { Cmd("Telescope help_tags"), "Search Help Tags" },
-			},
-
-			-- Marks
-			m = {
-				name = "Marks",
-				a = { Cmd('lua require("harpoon"):list():append()'), "Add Mark" },
-				r = { Cmd('lua require("harpoon"):list():remove()'), "Remove Mark" },
-				n = { Cmd('lua require("harpoon"):list():next()'), "Next Mark" },
-				p = { Cmd('lua require("harpoon"):list():prev()'), "Previous Mark" },
-				m = { Cmd("Telescope harpoon marks"), "Open Marks list" },
-			},
-
-			-- NvimTree
-			e = { Cmd("NvimTreeToggle"), "Toggle nvim-tree" },
-
-			-- Buffer Management
-			q = {
-				name = "Close",
-				Q = { Cmd("Bdelete!"), "Force close buffer" }, -- (uses bbye)
-				b = { Cmd("bd"), "Close buffer" },
-				o = { Cmd("BufferLineCloseOthers"), "Close all other buffers" }, -- (uses bufferline)
-				q = { Cmd("Bdelete"), "Close buffer" }, -- (uses bbye)
-				v = { Cmd("qall"), "Quit nvim" },
-			},
-			w = { Cmd("w!"), "Force write buffer" },
-			h = { Cmd("noh"), "Remove highlights" },
-
-			-- Get *
-			g = {
-				name = "Get something",
-				D = { Cmd("lua vim.lsp.buf.declaration()"), "Declaration" },
-				["/"] = { Cmd("Telescope current_buffer_fuzzy_find"), "Search current buffer" },
-				d = { Cmd("lua vim.lsp.buf.definition()"), "Definition" },
-				i = { Cmd("lua vim.lsp.buf.implementation()"), "Implementation" },
-				r = { Cmd("Telescope lsp_references"), "All references in code base" },
-			},
-
-			t = { Cmd("TroubleToggle"), "Toggle Trouble buffer" },
-			z = { Cmd("Telescope symbols"), "z-emojis of course" },
-			["/"] = { "<Plug>(comment_toggle_linewise_current)", "Comment toggle current line" },
-			["="] = { Cmd("wincmd ="), "Reset window distribution" },
-		}
-
-		local visual_mode_opts = {
+		},
+	},
+	keys = {
+		{
+			leader .. "/",
+			"<Plug>(comment_toggle_linewise_current)",
+			desc = "Comment toggle current line",
+			nowait = true,
+			remap = false,
+		},
+		{ leader .. "=", Cmd("wincmd ="), desc = "Reset window distribution", nowait = true, remap = false },
+		{
+			leader .. "G",
+			Cmd('lua require("telescope").extensions.live_grep_args.live_grep_args()'),
+			desc = "Grep text",
+			nowait = true,
+			remap = false,
+		},
+		{
+			leader .. "N",
+			Cmd("lua vim.diagnostic.goto_prev({ border = 'rounded' })"),
+			desc = "Jump to previous error",
+			nowait = true,
+			remap = false,
+		},
+		{
+			leader .. "R",
+			Cmd("Telescope resume"),
+			desc = "Resume last Telescope instance",
+			nowait = true,
+			remap = false,
+		},
+		{ leader .. "T", group = "Telescope", nowait = true, remap = false },
+		{
+			leader .. "Tc",
+			Cmd("Telescope command_history"),
+			desc = "Command History",
+			nowait = true,
+			remap = false,
+		},
+		{ leader .. "Th", Cmd("Telescope help_tags"), desc = "Search Help Tags", nowait = true, remap = false },
+		{ leader .. "Tt", Cmd("Telescope"), desc = "Toggle Telescope", nowait = true, remap = false },
+		{
+			leader .. "a",
+			Cmd("lua vim.lsp.buf.code_action()"),
+			desc = "Display code action",
+			nowait = true,
+			remap = false,
+		},
+		{ leader .. "b", Cmd("Telescope buffers"), desc = "Buffers", nowait = true, remap = false },
+		{ leader .. "d", group = "Git diff", nowait = true, remap = false },
+		{ leader .. "dc", Cmd("DiffviewClose"), desc = "close diff view", nowait = true, remap = false },
+		{ leader .. "df", Cmd("Telescope git_status hidden=true"), desc = "files", nowait = true, remap = false },
+		{ leader .. "do", Cmd("DiffviewOpen"), desc = "open diff view", nowait = true, remap = false },
+		{ leader .. "dp", Cmd("diffput"), desc = "put diff back", nowait = true, remap = false },
+		{ leader .. "e", Cmd("NvimTreeToggle"), desc = "Toggle nvim-tree", nowait = true, remap = false },
+		{
+			leader .. "f",
+			Cmd("Telescope find_files hidden=true"),
+			desc = "Find files",
+			nowait = true,
+			remap = false,
+		},
+		{ leader .. "g", group = "Get something", nowait = true, remap = false },
+		{
+			leader .. "g/",
+			Cmd("Telescope current_buffer_fuzzy_find"),
+			desc = "Search current buffer",
+			nowait = true,
+			remap = false,
+		},
+		{
+			leader .. "gD",
+			Cmd("lua vim.lsp.buf.declaration()"),
+			desc = "Declaration",
+			nowait = true,
+			remap = false,
+		},
+		{ leader .. "gd", Cmd("lua vim.lsp.buf.definition()"), desc = "Definition", nowait = true, remap = false },
+		{
+			leader .. "gi",
+			Cmd("lua vim.lsp.buf.implementation()"),
+			desc = "Implementation",
+			nowait = true,
+			remap = false,
+		},
+		{
+			leader .. "gr",
+			Cmd("Telescope lsp_references"),
+			desc = "All references in code base",
+			nowait = true,
+			remap = false,
+		},
+		{ leader .. "h", Cmd("noh"), desc = "Remove highlights", nowait = true, remap = false },
+		{ leader .. "k", group = "signature help", nowait = true, remap = false },
+		{ leader .. "kK", Cmd("lua vim.lsp.buf.hover()"), desc = "Show hover", nowait = true, remap = false },
+		{
+			leader .. "kh",
+			Cmd("lua vim.lsp.buf.signature_help()"),
+			desc = "Signature help",
+			nowait = true,
+			remap = false,
+		},
+		{
+			leader .. "kk",
+			Cmd("lua vim.diagnostic.open_float()"),
+			desc = "Show diagnostics",
+			nowait = true,
+			remap = false,
+		},
+		{ leader .. "m", group = "Marks", nowait = true, remap = false },
+		{
+			leader .. "ma",
+			Cmd('lua require("harpoon"):list():append()'),
+			desc = "Add Mark",
+			nowait = true,
+			remap = false,
+		},
+		{ leader .. "mm", Cmd("Telescope harpoon marks"), desc = "Open Marks list", nowait = true, remap = false },
+		{
+			leader .. "mn",
+			Cmd('lua require("harpoon"):list():next()'),
+			desc = "Next Mark",
+			nowait = true,
+			remap = false,
+		},
+		{
+			leader .. "mp",
+			Cmd('lua require("harpoon"):list():prev()'),
+			desc = "Previous Mark",
+			nowait = true,
+			remap = false,
+		},
+		{
+			leader .. "mr",
+			Cmd('lua require("harpoon"):list():remove()'),
+			desc = "Remove Mark",
+			nowait = true,
+			remap = false,
+		},
+		{
+			leader .. "n",
+			Cmd("lua vim.diagnostic.goto_next({ border = 'rounded' })"),
+			desc = "Jump to next error",
+			nowait = true,
+			remap = false,
+		},
+		{ leader .. "q", group = "Close", nowait = true, remap = false },
+		{ leader .. "qQ", Cmd("Bdelete!"), desc = "Force close buffer", nowait = true, remap = false },
+		{ leader .. "qb", Cmd("bd"), desc = "Close buffer", nowait = true, remap = false },
+		{
+			leader .. "qo",
+			Cmd("BufferLineCloseOthers"),
+			desc = "Close all other buffers",
+			nowait = true,
+			remap = false,
+		},
+		{ leader .. "qq", Cmd("Bdelete"), desc = "Close buffer", nowait = true, remap = false },
+		{ leader .. "qv", Cmd("qall"), desc = "Quit nvim", nowait = true, remap = false },
+		{
+			leader .. "r",
+			Cmd("lua vim.lsp.buf.rename()"),
+			desc = "Rename all instances",
+			nowait = true,
+			remap = false,
+		},
+		{
+			leader .. "s",
+			Cmd("Telescope current_buffer_fuzzy_find"),
+			desc = "Search current buffer",
+			nowait = true,
+			remap = false,
+		},
+		{ leader .. "t", Cmd("TroubleToggle"), desc = "Toggle Trouble buffer", nowait = true, remap = false },
+		{ leader .. "w", Cmd("w!"), desc = "Force write buffer", nowait = true, remap = false },
+		{ leader .. "z", Cmd("Telescope symbols"), desc = "z-emojis of course", nowait = true, remap = false },
+		{
+			leader .. "/",
+			"<Plug>(comment_toggle_linewise_visual)",
+			desc = "Comment toggle current highlight",
 			mode = "x",
-			prefix = " ",
-			buffer = nil, -- Global mappings. Specify a buffer number for buffer local mappings
-			silent = true, -- use `silent` when creating keymaps
-			noremap = true, -- use `noremap` when creating keymaps
-			nowait = true, -- use `nowait` when creating keymaps
-		}
-
-		local visual_mode_mappings = {
-			["/"] = { "<Plug>(comment_toggle_linewise_visual)", "Comment toggle current highlight" },
-		}
-
-		which_key.setup(setup_preferences)
-		which_key.register(normal_mode_mappings, normal_mode_opts)
-		which_key.register(visual_mode_mappings, visual_mode_opts)
-	end,
+			nowait = true,
+			remap = false,
+		},
+	},
 }
