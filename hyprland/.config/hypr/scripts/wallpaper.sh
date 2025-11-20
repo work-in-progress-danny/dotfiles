72aa962a2630d3fe985b27ac63637032a7fda72c
Danny Lowater <dannylow456@gmail.com>
2025-11-20 12:33:50 +1100

    feat: move over to using awww instead of swww

--- hyprland/.config/hypr/scripts/wallpaper.sh
+++ hyprland/.config/hypr/scripts/wallpaper.sh
@@ -1,21 +1,8 @@
-#!/usr/bin/env sh
-
-# Lock file to prevent multiple instances
-lockFile="/tmp/wallpaper_lock_$(id -u).lock"
-[ -e "$lockFile" ] && echo "An instance is already running..." && exit 1
-touch "$lockFile"
-trap 'rm -f "$lockFile"' EXIT
-
-# Directory containing wallpapers
 WALLPAPER_DIR="$HOME/dotfiles/wallpapers"
 
-# Pick a random image
 IMAGE=$(find "$WALLPAPER_DIR" -type f \( -iname '*.jpg' -o -iname '*.png' -o -iname '*.jpeg' \) | shuf -n 1)
 
-# Set the wallpaper with swww, stretching to fit the screen
 if [ -n "$IMAGE" ]; then
-    swww img "$IMAGE" --resize stretch --filter Lanczos3
-else
-    echo "No wallpapers found in $WALLPAPER_DIR"
+    awww img "$IMAGE"
 fi
 
