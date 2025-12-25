7e378639952784f977cb8986c4dd0815d00c34a0
Danny Lowater <dannylow456@gmail.com>
2025-12-26 09:16:31 +1100

    feat: fix nvim surround and update treesitter to use new syntax

--- nvim/.config/nvim/lua/plugins/nvim-surround.lua
+++ nvim/.config/nvim/lua/plugins/nvim-surround.lua
@@ -1,4 +1,8 @@
 return {
 	"kylechui/nvim-surround",
-	dependencies = { "nvim-treesitter/nvim-treesitter-textobjects" },
+	version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+	event = "VeryLazy",
+	config = function()
+		require("nvim-surround").setup({})
+	end,
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,48 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
-	opts = function()
-		require("nvim-treesitter.configs").setup({
-			ensure_installed = "all",
-			auto_install = true,
-		})
-	end,
+	main = "nvim-treesitter.configs", -- Important: tells Lazy to require this module for opts
+	opts = {
+		ensure_installed = {
+			-- Core JS/TS ecosystem
+			"typescript",
+			"tsx",
+			"javascript",
+
+			-- Markup and styles
+			"html",
+			"css",
+			"scss",
+
+			-- Config/data formats
+			"json",
+			"jsonc",
+			"jsdoc",
+
+			-- Markdown
+			"markdown",
+			"markdown_inline",
+
+			-- Common full-stack additions
+			"bash",
+			"dockerfile",
+			"gitattributes",
+			"gitignore",
+			"git_rebase",
+			"graphql",
+			"regex",
+			"toml",
+			"yaml",
+
+			-- Neovim essentials
+			"lua",
+			"vim",
+			"vimdoc",
+			"query",
+		},
+
+		highlight = { enable = true },
+		indent = { enable = true },
+		auto_install = true,
+	},
 }
